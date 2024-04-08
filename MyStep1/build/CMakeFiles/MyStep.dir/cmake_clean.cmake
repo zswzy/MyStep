7cb4d9a54f3092e8dd1381d46a44b0d8d6539d65
Zeyuan Wang <41506423+zswzy@users.noreply.github.com>
2024-04-08 15:55:02 +0200

    Fisrt Commit MyStep1

---
+++ MyStep1/build/CMakeFiles/MyStep.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/zeyua/Documents/Personal Project/cpp tests/cmake-3.29.1-tutorial-source/cmake-3.29.1-tutorial-source/MyStep/bin/MyStep.exe"
+  "C:/Users/zeyua/Documents/Personal Project/cpp tests/cmake-3.29.1-tutorial-source/cmake-3.29.1-tutorial-source/MyStep/bin/MyStep.exe.manifest"
+  "C:/Users/zeyua/Documents/Personal Project/cpp tests/cmake-3.29.1-tutorial-source/cmake-3.29.1-tutorial-source/MyStep/bin/MyStep.pdb"
+  "C:/Users/zeyua/Documents/Personal Project/cpp tests/cmake-3.29.1-tutorial-source/cmake-3.29.1-tutorial-source/MyStep/bin/libMyStep.dll.a"
+  "CMakeFiles/MyStep.dir/src/main.cpp.obj"
+  "CMakeFiles/MyStep.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyStep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
