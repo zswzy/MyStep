e39d06df699d2d07e1cefc409d657254de8a1256
Zeyuan Wang <41506423+zswzy@users.noreply.github.com>
2024-04-08 23:32:18 +0200

    add step2: debugging use vscode

---
+++ MyStep2/build/CMakeFiles/MyStep.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyStep.
+# This may be replaced when dependencies are built.
---
+++ MyStep2/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    double temp = 10;
+    std::cout << "Hello cpp: " << temp  << std::endl;
+    return 0;
+}
